3c3835acaceb839862c663b09debe3b25fbb5bcb
pitang1965 <pitang1965@gmail.com>
2022-08-09 17:30:44 +0900

    🎨 Add baseUrl and create src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from 'styles/Home.module.css';
 import {
   Anchor,
   AppShell,
