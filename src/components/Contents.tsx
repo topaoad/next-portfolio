c6b692b9f02df6f97b0888d8611d457e6f8eb3c6
pitang1965 <pitang1965@gmail.com>
2022-08-10 17:36:34 +0900

    🎨 Add Contents.tsx

---
+++ src/components/Contents.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Container } from '@mantine/core';
+import { TitleSection } from 'src/components/TitleSection';
+import styles from './Contents.module.css';
+
+export const Contents = () => {
+  return (
+    <Container className={styles.container}>
+      {' '}
+      <TitleSection name='ピータン' />
+      <div className={styles.grid}>
+        <a href='https://nextjs.org/docs' className={styles.card}>
+          <h2>Documentation &rarr;</h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a href='https://nextjs.org/learn' className={styles.card}>
+          <h2>Learn &rarr;</h2>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
+
+        <a
+          href='https://github.com/vercel/next.js/tree/canary/examples'
+          className={styles.card}
+        >
+          <h2>Examples &rarr;</h2>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </a>
+
+        <a
+          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          className={styles.card}
+        >
+          <h2>Deploy &rarr;</h2>
+          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+        </a>
+      </div>
+    </Container>
+  );
+};
