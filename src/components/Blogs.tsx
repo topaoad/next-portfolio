c8931a771608e7d236524813a5f210457da05b82
pitang1965 <pitang1965@gmail.com>
2022-08-13 11:53:44 +0900

    🐛 Number of blogs displyed on PC

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -3,11 +3,16 @@ import { Container, Stack } from '@mantine/core';
 import { blogData } from 'data/blogData';
 import { BlogCard } from './BlogCard';
 
+import { useAtom } from 'jotai';
+import { isMobileUiAtom } from 'src/atoms/uiMode';
+
 export const Blogs = () => {
+  const [isMobileUi] = useAtom(isMobileUiAtom);
+  const numberToShow = isMobileUi ? 4 : 5;
   return (
     <Container fluid>
       <Stack spacing='xl' className='styles.container'>
-        {blogData.slice(0, 4).map((blog) => <BlogCard data={blog} key={blog.id} />)}
+        {blogData.slice(0, numberToShow).map((blog) => <BlogCard data={blog} key={blog.id} />)}
       </Stack>
     </Container>
   );
