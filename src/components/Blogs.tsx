89b1978004db4ecb6c85c491d5b5c3594ec4882b
pitang1965 <pitang1965@gmail.com>
2022-08-14 17:02:55 +0900

    💄 Chage number of blog in PC mode

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+
 import { Container, Stack } from '@mantine/core';
 import { blogData } from 'data/blogData';
 import { BlogCard } from './BlogCard';
@@ -7,8 +9,15 @@ import { useAtom } from 'jotai';
 import { isMobileUiAtom } from 'src/atoms/uiMode';
 
 export const Blogs = () => {
+  // ブログをいくつ表示するかどうか（モバイル表示かどうか、ホームページかどうかで異なる）
+  const router = useRouter();
+  let numberToShow;
   const [isMobileUi] = useAtom(isMobileUiAtom);
-  const numberToShow = isMobileUi ? 4 : 5;
+  if (router.pathname === '/') {
+    numberToShow = isMobileUi ? 4 : 5;
+  } else {
+    numberToShow = isMobileUi ? 5 : 10;
+  }
 
   return (
     <Container fluid>
