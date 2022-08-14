bfcc4c233c858abe589d65009d6dcffd40ab6f17
pitang1965 <pitang1965@gmail.com>
2022-08-14 11:14:31 +0900

    ⚰️ Remove unused css file

--- src/components/GitHubSection.tsx
+++ src/components/GitHubSection.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './GitHubSection.module.css';
 import { Center, Container, Divider, Stack, Title } from '@mantine/core';
 import { LinkButton } from './LinkButton';
 import { GitHubRepos } from './GitHubRepos';
@@ -8,7 +7,7 @@ const githubUrl = 'https://github.com/pitang1965?tab=repositories';
 
 export const GitHubSection = () => {
   return (
-    <Container className={styles.container}>
+    <Container>
       <Stack spacing='lg'>
         <Title align='left'>GitHub</Title>
         <Divider mt='sm' />
