15e3e76f8ba53740d4101780abd9601d4bce8e43
pitang1965 <pitang1965@gmail.com>
2022-08-15 10:53:15 +0900

    ✨ Implement SnsIconGroup.tsx about hyperlink etc.

--- src/components/SnsIconGroup.tsx
+++ src/components/SnsIconGroup.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { ActionIcon, Group } from '@mantine/core';
-
+import { ActionIcon, Group, Tooltip } from '@mantine/core';
 import { TwitterIcon } from './icons/TwitterIcon';
 import { FacebookIcon } from './icons/FacebookIcon';
 import { RssIcon } from './icons/RssIcon';
+import { twitterUrl } from 'data/urlData';
 
 export const SnsIconGroup = () => {
   return (
     <Group position='left' spacing='xs'>
-      <ActionIcon variant='transparent'>
+      <ActionIcon variant='transparent' component='a' href={twitterUrl}>
         <TwitterIcon />
       </ActionIcon>
-      <ActionIcon variant='transparent' color='yellow'>
-        <FacebookIcon />
-      </ActionIcon>
-      <ActionIcon variant='transparent' color='yellow'>
-        <RssIcon />
-      </ActionIcon>
+      <Tooltip label='秘密だよ'>
+        <ActionIcon variant='transparent' color='yellow'>
+          <FacebookIcon />
+        </ActionIcon>
+      </Tooltip>
+      <Tooltip label='未実装だよ'>
+        <ActionIcon variant='transparent' color='yellow'>
+          <RssIcon />
+        </ActionIcon>
+      </Tooltip>
     </Group>
   );
 };
